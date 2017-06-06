6157fad3c43741a99d87db3bb0659e093710e252
zondahuman <zondahuman@gmail.com>
2017-06-06 22:27:18 +0800

    add

--- com/abin/lee/thread/config/RabbitmqConfig.py
+++ com/abin/lee/thread/config/RabbitmqConfig.py
@@ -7,9 +7,9 @@ class RabbitmqEnum(Enum):
     RABBITMQ_USER = "guest"
     RABBITMQ_PWD = "RRD"
     RABBITMQ_QUEUE = "audit.response.tl.queue"
+    RABBITMQ_ROUTING_KEY = "audit.response.tl.queue"
+    RABBITMQ_EXCHANGE = "audit.exchange"
     RABBITMQ_HOST = "172.16.2.145"
     RABBITMQ_PORT = 15672
     RABBITMQ_VIRTUAL_HOST = "/"
     RABBITMQ_VIRTUAL_HOST = "/"
-    YM = "YM"
-    TL = "TL"
